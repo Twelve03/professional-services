4cc3d090db34d2a30deae908613584c9db83f5b0
Twelve03 <84999346+Twelve03@users.noreply.github.com>
2021-08-13 18:08:54 -0400

    Landing page structure

---
+++ src/assets/js/script.js
@@ -0,0 +1,7 @@
+// Global functions
+function navToggle() {
+  const nav = document.getElementById("menu");
+  const mask = document.getElementById("mask");
+  nav.classList.toggle("hidden");
+  mask.classList.toggle("hidden");
+}
